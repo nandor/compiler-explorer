84c2c9001879e81e1a26ae85136beb28953372a2
Nandor Licker <n@ndor.email>
2021-11-18 07:56:45 +0200

    react skeleton

--- src/index.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-  var editor = ace.edit("source");
-  editor.setTheme("ace/theme/monokai");
-  editor.session.setMode("ace/mode/javascript");
-});
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,41 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "src", "index.html")
-    })
-  ]
+module.exports = function(env, argv) {
+  const isProduction = argv.mode === "production";
+
+  return {
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "src", "index.html")
+      })
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              cacheDirectory: true,
+              cacheCompression: false,
+              envName: argv.mode
+            }
+          }
+        },
+        {
+          test: /\.css$/,
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : "style-loader",
+            "css-loader"
+          ]
+        }
+      ]
+    },
+    resolve: {
+      extensions: [".js", ".jsx"]
+    }
+  };
 };
